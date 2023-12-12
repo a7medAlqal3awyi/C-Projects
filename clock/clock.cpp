41a24fc47d2e0aa991c7898b39be9557ad072ddc
Ahmed <a7medalqalawyi@bmail.com>
2023-12-12 23:42:57 +0200

    finish clock app

---
+++ clock/clock.cpp
@@ -0,0 +1,33 @@
+#include <iostream >
+#include <windows.h>
+using namespace std;
+int main()
+{
+    int hrs, min, sec;
+    cout << "Hours\n";
+    cin >> hrs;
+    cout << "Minits\n";
+    cin >> min;
+    cout << "Seconds\n";
+    cin >> sec;
+    while (true)
+    {
+        system("cls");
+        if (sec > 59)
+        {
+            min++;
+            sec = 0;
+        }
+        if (min > 59)
+        {
+            hrs++;
+            min = 0;
+        }
+        if (hrs > 23)
+
+            hrs = 0;
+        cout << hrs << " : " << min << " : " << sec << endl;
+        sec++;
+        Sleep(900);
+    }
+}
